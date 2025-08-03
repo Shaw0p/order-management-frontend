e849d4ad082b9bdb2d821922376fb95963ce3e19
Shaw0p <assh23ise@cmrit.ac.in>
2025-08-04 01:05:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
           <Route path="/" element={<OrderList />} />
           <Route path="/create" element={<OrderForm />} />
           <Route path="/orders/:id" element={<OrderDetails />} />
+          <Route path="/invoices" element={<InvoicesPage />} />
+
         </Routes>
       </div>
     </Router>
