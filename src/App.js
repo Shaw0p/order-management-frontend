f6ab7174564abbd3013afbb876f73b6d909177e0
Shaw0p <assh23ise@cmrit.ac.in>
2025-08-04 22:57:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import "./App.css";
 
 import OrderForm from "./components/OrderForm";
 import OrderList from "./components/OrderList";
@@ -7,25 +9,27 @@ import OrderDetails from "./components/OrderDetails";
 import AdminPanel from "./components/AdminPanel";
 import Login from "./components/Login";
 
-
 function App() {
+  const isAdmin = localStorage.getItem("isAdmin") === "true";
+
   return (
     <Router>
-      <div>
-        <nav>
-          <Link to="/">Orders</Link> | 
-          <Link to="/create">Create Order</Link> | 
-          <Link to="/admin">Admin Panel</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<OrderList />} />
-          <Route path="/create" element={<OrderForm />} />
-          <Route path="/orders/:id" element={<OrderDetails />} />
-          <Route path="/admin" element={<AdminPanel />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+      <div className="navbar">
+        <Link to="/">Orders</Link> | 
+        <Link to="/create">Create Order</Link> | 
+        <Link to="/admin">Admin</Link>
       </div>
+
+      <Routes>
+        <Route path="/" element={<OrderList />} />
+        <Route path="/create" element={<OrderForm />} />
+        <Route path="/orders/:id" element={<OrderDetails />} />
+        <Route path="/admin" element={<Login />} />
+        <Route
+          path="/admin-panel"
+          element={isAdmin ? <AdminPanel /> : <Navigate to="/admin" />}
+        />
+      </Routes>
     </Router>
   );
 }
